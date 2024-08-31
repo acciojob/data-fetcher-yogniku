57683f875493df04b6b72f542a2e500c45d8b87f
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-08-31 17:35:05 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Products from "./Products";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Products/>
     </div>
   )
 }
