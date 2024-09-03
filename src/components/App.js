d0e9fa0dc254a94d986893b41af8e437260edec9
Yogesh3712 <113026686+Yogesh3712@users.noreply.github.com>
2024-09-03 20:48:57 +0530

    chnages done

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,95 @@
+import React, { useEffect, useState } from "react";
 
-import React from "react";
 import './../styles/App.css';
-import Products from "./Products";
+
+ 
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        <Products/>
-    </div>
-  )
-}
 
-export default App
+  const [text, setText] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+
+ 
+
+  useEffect(() => {
+
+    fetch("https://dummyjson.com/products")
+
+      .then((response) => {
+
+        if (!response.ok) {
+
+          throw new Error("Network response was not ok");
+
+        }
+
+        return response.json();
+
+      })
+
+      .then((json) => {
+
+        setText(json);
+
+        setLoading(false);
+
+      })
+
+      .catch((error) => {
+
+        setError(error.message);
+
+        setLoading(false);
+
+      });
+
+  }, []);
+
+ 
+
+  if (loading) {
+
+    return <h1>Loading...</h1>;
+
+  }
+
+ 
+
+  if (error) {
+
+    return <h1>An error occurred: {error}</h1>;
+
+  }
+
+ 
+
+  return (
+
+    <div>
+
+      <h1>
+
+      Data Fetched from API
+
+      </h1>
+
+ 
+
+      <pre>
+
+        {JSON.stringify(text, null, 2)}
+
+      </pre>
+
+    </div>
+
+  );
+
+};
+
+ 
+
+export default App;
